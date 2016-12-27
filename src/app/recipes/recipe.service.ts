05b2b61b1207dd467d9e88e8f60152d145938c9a
Stefan Trajkovic <stefan.trajkovic@fusion.local>
2016-12-27 12:45:28 +0100

    recipe-edit form builded

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -13,10 +13,10 @@ private	recipes: Recipe[] = [
     new Recipe('Summer Salad', 'Okayish', 'http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg', [])
   ];
 
-  constructor() { }
+  constructor() {}
 
   getRecipes() {
-  	return this.recipes;
+    return this.recipes;
   }
 
   getRecipe(id: number) {
@@ -25,6 +25,14 @@ private	recipes: Recipe[] = [
 
   deleteRecipe(recipe: Recipe) {
     this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
   }
 
 }
