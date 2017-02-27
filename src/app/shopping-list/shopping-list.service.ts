956af143479256cdb3fa90d25030b56b1a0910c0
saibot777 <stefantrajkovic777@gmail.com>
2017-02-27 11:37:58 +0100

    shopping list form ready

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,7 +3,7 @@ import {Ingredient} from "../shared/ingredient";
 export class ShoppingListService {
   private items: Ingredient[] = [];
 
-  constructor() { }
+  constructor() {}
 
   getItems() {
     return this.items;
@@ -12,4 +12,17 @@ export class ShoppingListService {
   addItems(items: Ingredient[]) {
     Array.prototype.push.apply(this.items, items);
   }
+
+  addItem(item: Ingredient) {
+    this.items.push(item);
+  }
+
+  editItem(oldItem: Ingredient, newItem: Ingredient) {
+    this.items[this.items.indexOf(oldItem)] = newItem;
+  }
+
+  deleteItem(item: Ingredient) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+
 }
