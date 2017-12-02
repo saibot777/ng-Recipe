7fb736c2f59826ebb2e53a3f5a4a9187f540e30f
saibot777 <stefantrajkovic777@gmail.com>
2017-12-02 14:12:52 +0100

    update and delete state ingredient

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -6,7 +6,9 @@ const initialState = {
   ingredients: [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
-  ]
+  ],
+  editedIngredient: null,
+  editedIngredientIndex: -1
 };
 
 export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
@@ -21,6 +23,25 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
                 ...state,
                 ingredients: [...state.ingredients, ...action.payload]
             };
+        case ShoppingListActions.UPDATE_INGREDIENT:
+            const ingredient = state.ingredients[action.payload.index];
+            const updatedIngredient = {
+                ...ingredient,
+                ...action.payload.ingredient
+            };
+            const ingredients = [...state.ingredients];
+            ingredients[action.payload.index] = updatedIngredient
+            return {
+                ...state,
+                ingredients: ingredients
+            };
+        case ShoppingListActions.DELETE_INGREDIENT:
+            const oldIngredients = [...state.ingredients];
+            oldIngredients.splice(action.payload, 1);
+            return {
+                ...state,
+                ingredients: oldIngredients
+            }
         default:
             return state;
     }
