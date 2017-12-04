d3af627d2f0fbb19c5a869555ae7c1f304db34f0
saibot777 <stefantrajkovic777@gmail.com>
2017-12-04 12:54:00 +0100

    authentication stream implemented, authService refactored

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,14 +1,18 @@
+import { Store } from '@ngrx/store';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
+import * as fromApp from '../store/app.reducers';
 
-import { AuthService } from './auth.service';
+import * as fromAuth from './store/auth.reducer';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.authService.isAuthenticated();
+    return this.store.select('auth').map((authState: fromAuth.State) => {
+      return authState.authenticated;
+    })
   }
 }
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,13 +1,35 @@
+import * as AuthActions from './auth.actions';
+
 export interface State {
-    token: string;
-    authenticated: boolean;
+  token: string;
+  authenticated: boolean;
 }
- 
+
 const initialState: State = {
-   token: null,
-   authenticated: false
+  token: null,
+  authenticated: false
 };
- 
-export function authReducer(state = initialState, action) {
-    return state;
+
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case (AuthActions.SIGNUP):
+    case (AuthActions.SIGNIN):
+      return {
+        ...state,
+        authenticated: true
+      };
+    case (AuthActions.LOGOUT):
+      return {
+        ...state,
+        token: null,
+        authenticated: false
+      };
+    case (AuthActions.SET_TOKEN):
+      return {
+        ...state,
+        token: action.payload
+      }
+    default:
+      return state;
+  }
 }
